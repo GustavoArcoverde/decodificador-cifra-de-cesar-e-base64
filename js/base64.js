f6f367dfa22c152963f7eebe9cd74c0f617f1cd4
GustavoArcoverde <garcoverde3@gmail.com>
2022-04-04 20:01:27 -0300

    add botão de submit para codificar e decodificar texto do usuario

--- js/base64.js
+++ js/base64.js
@@ -1,16 +1,28 @@
 var base64 = document.getElementById('base64')
-var textoInput = "Gustavasso"
-var submit = document.getElementById('submit')
 var textoFinal = document.getElementById('textoFinal')
+var submit = document.getElementById('submit')
+var codificar = document.getElementById('codificar')
+var decodificar = document.getElementById('decodificar')
 
 function codificaTexto(){
+    var textoInput = document.getElementById('textoInicial').value
     var textoCodificado = btoa(textoInput)
     textoFinal.innerText = textoCodificado
 }
 
-function decodificaTexto(){}
+function decodificaTexto(){
+    var textoInput = document.getElementById('textoInicial').value
+    var textoDecodificado = atob(textoInput)
+    textoFinal.innerText = textoDecodificado
+}
 
 submit.addEventListener('click', function(){
-    codificaTexto()
+    if(codificar.checked){
+        codificaTexto()
+    }else if(decodificar.checked){
+        decodificaTexto()
+    }else{
+        alert("Escolha uma das opções")
+    }
 })
 
